4e1614936b5c6160123c1bbb4235e6b9fc99c898
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-05 18:47:29 -0500

    created all of the models

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,19 +3,19 @@ const router = require("express").Router();
 const blogPosts = [
     {
         username: "JohnDoe23",
-        blog_title: "Why is MVC so important?",
+        title: "Why is MVC so important?",
         description: "Overall, using the MVC pattern in JavaScript web applications can help developers create more maintainable, reusable, and scalable code, which ultimately leads to a better user experience."
     },
 
     {
         username: "BillGates7",
-        blog_title: "Are CSS frameworks cheating or essential?",
+        title: "Are CSS frameworks cheating or essential?",
         description: "CSS frameworks is not cheating, but they are helpful in building websites and applications quickly and efficiently. However, it's important to weigh the pros and cons and decide if a particular framework is suitable for the project's requirements and the developer's expertise. It's also essential to be aware of the framework's limitations and bloat and choose the appropriate framework for the project."
     },
 
     {
         username: "Steve_needs_a_Job",
-        blog_title: "How should AI etiquette be maintained?",
+        title: "How should AI etiquette be maintained?",
         description: "Overall, I believe maintaining AI etiquette involves following ethical principles that promote fairness, transparency, privacy, accountability, and human oversight in the development and deployment of AI systems. By adhering to these principles, we can ensure that AI is used in a way that benefits society and protects individual rights and freedoms....But I do believe this is difficult to achieve. Thoughts?"
     }
 ]
