9e451ea3e058b624940738bbca3de1a7a9605cb9
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-03-05 18:57:24 -0500

    created index.js model/associations and modified Posts model

--- models/Comments.js
+++ models/Comments.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Comment extends Model {}
+class Comments extends Model {}
 
-Comment.init(
+Comments.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -42,8 +42,8 @@ Comment.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'comment',
+    modelName: 'comments',
   }
 );
 
-module.exports = Comment;
+module.exports = Comments;
---
+++ models/index.js
@@ -0,0 +1,32 @@
+// models/index.js
+
+const User = require('./User');
+const Posts = require('./Posts');
+const Comments = require('./Comments');
+
+// associations
+User.hasMany(Posts, {
+  foreignKey: 'user_id',
+});
+
+Posts.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+User.hasMany(Comments, {
+    foreignKey: 'user_id',
+});
+
+Comments.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Posts.hasMany(Comments, {
+  foreignKey: 'post_id',
+});
+
+Comments.belongsTo(Posts, {
+  foreignKey: 'post_id',
+});
+
+module.exports = { User, Posts, Comments };
